1c072b5d8e94f5d3829c80828341a4c9bf9135fc
pitikdmitry <pitik.dmitry@mail.ru>
2019-05-10 21:55:29 +0300

    generate tight brackets sequences

--- 3.py
+++ 3.py
@@ -1,62 +1,16 @@
-# New Year Chaos
-def swap(q, i, j):
-    temp = q[i]
-    q[i] = q[j]
-    q[j] = temp
-    return q
+def generate_parenthesis(n, arr) -> None:
+    if n == len(arr):
+        print(arr)
+    else:
+        arr.append('(')
+        generate_parenthesis(n, arr)
+        arr.pop()
+
+        arr.append(')')
+        generate_parenthesis(n, arr)
+        arr.pop()
 
 
-# def countBribes(q, counter, bribes):
-#     need_recursion = False
-#     for i in range(1, len(q)):
-#         if q[i] < q[i - 1]:
-#             q = swap(q, i, i - 1)
-#             bribes += 1
-#             counter += 1
-#             need_recursion = True
-#             if counter >= 3:
-#                 print("Too chaotic")
-#                 return
-#         else:
-#             counter = 0
-#
-#     if need_recursion:
-#         return countBribes(q, 0, bribes)
-#     else:
-#         print(bribes)
-#         return bribes
-
-
-def countBribes(q, counter, bribes):
-    # need_recursion = False
-    for i in range(1, len(q)):
-        if q[i] < q[i - 1]:
-            q = swap(q, i, i - 1)
-            bribes += 1
-            counter += 1
-            i = 0
-            # need_recursion = True
-            if counter >= 3:
-                print("Too chaotic")
-                return
-        else:
-            counter = 0
-
-    # if need_recursion:
-    #     return countBribes(q, 0, bribes)
-    # else:
-    #     print(bribes)
-    #     return bribes
-
-
-def minimumBribes(q: []):
-    counter = 0
-    bribes = 0
-    countBribes(q, counter, bribes)
-
-
-t = int(input())
-for i in range(t):
-    n = int(input())
-    arr = list(map(int, input().split()))
-    bribes = minimumBribes(arr)
+arr = []
+n = 4
+generate_parenthesis(n, arr)
