360371cf0fa4980a0a56c9c438db8254963164e1
pitikdmitry <pitik.dmitry@mail.ru>
2019-05-10 21:57:39 +0300

    binary search in rotated array

--- 4.py
+++ 4.py
@@ -1,29 +1,69 @@
-# Hash Tables: Ransom Note
-n, m = map(int, input().split())
+# binary search in rotated array
 
-magazine = input().split()
-note = input().split()
 
-words = {}
-for word in magazine:
-    if word not in words:
-        words[word] = 1
-    else:
-        words[word] += 1
+class Solution:
 
-error = False
+    def search_right(self, nums: [], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
 
-for word in note:
-    if word not in words:
-        error = True
-        print('No')
-        break
-    elif words[word] < 1:
-        error = True
-        print('No')
-        break
-    else:
-        words[word] -= 1
+        while left <= right:
+            mid = (left + right) // 2
 
-if not error:
-    print('Yes')
+            if nums[mid] == target:
+                if mid == len(nums) - 1:
+                    return mid
+
+                if nums[mid] == nums[mid + 1]:
+                    left = mid + 1
+                    continue
+                else:
+                    return mid
+
+            if target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+
+    def search_left(self, nums: [], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                if mid == 0:
+                    return mid
+
+                if nums[mid] == nums[mid - 1]:
+                    right = mid - 1
+                    continue
+                else:
+                    return mid
+
+            if target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+
+    def search_range(self, nums: [], target: int) -> []:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        right_idx = self.search_right(nums, target)
+        left_idx = self.search_left(nums, target)
+        return [left_idx, right_idx]
+
+
+s = Solution()
+nums = [5, 7, 7, 8, 8, 10]
+target = 8
+res = s.search_range(nums, target)
+print(res)
